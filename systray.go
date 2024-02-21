af12ffc4b1c14a82a03b19db009d0a7516deccd2
43h <cw.clx@outlook.com>
2024-02-21 23:29:53 +0800

    去掉去窗口

--- systray.go
+++ systray.go
@@ -33,7 +33,7 @@ func onReady() {
 			time.Sleep(1 * time.Second)
 			t := time.Now()
 			sec := time.Now().Second()
-			str := fmt.Sprintf(t.Format("time 15:04:05"))
+			str := fmt.Sprintf(t.Format("15:04:05"))
 			if status == 2 {
 				return
 			} else if status == 0 {
@@ -49,19 +49,26 @@ func onReady() {
 				}
 				down := 0
 				up := 0
+				badip := ""
 				for i := 0; i < total; i++ {
 					if ips[i].avgRtt == -1 {
 						down += 1
+						if down < 3 { //只能显示3个down掉的IP
+							badip += ips[i].ipv4.To4().String() + " "
+						} else if down < 4 {
+							badip += ips[i].ipv4.To4().String()
+						}
 					} else {
 						up += 1
 					}
 				}
-				str += fmt.Sprintf("\nUp %d\nDown %d", up, down)
+				str += fmt.Sprintf("\nUp:%d Down:%d", up, down)
 
 				if up == total {
 					systray.SetIcon(picok)
 				} else {
 					systray.SetIcon(picnok)
+					str += "\n" + badip
 				}
 				systray.SetTooltip(str)
 			}
@@ -74,17 +81,14 @@ func onReady() {
 		systray.SetTitle("Awesome App")
 		systray.SetTooltip("Working...")
 
-		mMainwin := systray.AddMenuItem("MainWin", "MainWin")
 		mEnabled := systray.AddMenuItem("Enabled", "Enabled")
 		mDisabled := systray.AddMenuItem("Disabled", "Disabled")
-		systray.AddMenuItem("Version:2024-02-20", "Ignored")
+		systray.AddMenuItem("Version:2024-02-22", "Ignored")
 		systray.AddSeparator()
 		mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 
 		for {
 			select {
-			case <-mMainwin.ClickedCh:
-				go winmain()
 			case <-mEnabled.ClickedCh:
 				status = 1
 			case <-mDisabled.ClickedCh:
